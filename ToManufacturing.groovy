29c0a3b65c21673020842e8d44b5ba562d1e5676
Kevin Harrington <mad.hephaestus@gmail.com>
2017-03-11 17:50:52 -0500

    adding code that demostrates to mfg

--- ToManufacturing.groovy
+++ ToManufacturing.groovy
@@ -1,1 +1,9 @@
-//Your code here
+// Make a cube and put it up in the air
+CSG cubeInTheAir = new Cube(10).toCSG()
+				.movez(30)
+				.rotx(15)// rotate so it would not print well
+cubeInTheAir.setManufacturing({ toMfg ->
+	return toMfg
+			.rotx(-15)// fix the orentation
+			.toZMin()//move it down to the flat surface
+})				
