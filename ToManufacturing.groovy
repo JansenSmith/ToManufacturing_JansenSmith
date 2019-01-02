cfdac9efc2a7597e6697e56181185643824298fb
Kevin Harrington <mad.hephaestus@gmail.com>
2019-01-02 16:28:33 -0500

    adding export formats

--- ToManufacturing.groovy
+++ ToManufacturing.groovy
@@ -2,6 +2,8 @@
 CSG cubeInTheAir = new Cube(10).toCSG()
 				.movez(30)
 				.rotx(15)// rotate so it would not print well
+cubeInTheAir.addExportFormat("stl")// make an stl of the object
+cubeInTheAir.addExportFormat("svg")// make an svg of the object
 cubeInTheAir.setManufacturing({ toMfg ->
 	return toMfg
 			.rotx(-15)// fix the orentation
