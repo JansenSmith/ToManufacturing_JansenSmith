f4e9a111d919e29c714e651345fad47610f52f8b
Kevin Harrington <harrington@wpi.edu>
2019-01-15 14:11:11 -0500

    addingmthe set name

--- ToManufacturing.groovy
+++ ToManufacturing.groovy
@@ -2,6 +2,7 @@
 CSG cubeInTheAir = new Cube(10).toCSG()
 				.movez(30)
 				.rotx(15)// rotate so it would not print well
+cubeInTheAir.setName("CubeInTheAir")
 cubeInTheAir.addExportFormat("stl")// make an stl of the object
 cubeInTheAir.addExportFormat("svg")// make an svg of the object
 cubeInTheAir.setManufacturing({ toMfg ->
